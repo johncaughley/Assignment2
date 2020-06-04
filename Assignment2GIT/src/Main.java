b41e02fa6f29b3a3bbfb3406cca638f23f88ea2a
johncaughley <johncaughley1@gmail.com>
2020-06-04 18:37:16 +1200

    Version 1

--- Assignment2GIT/src/Main.java
+++ Assignment2GIT/src/Main.java
@@ -2,7 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Hi, my name is John");
+		System.out.println("Hi, my name is John");
+		System.out.println("My Favourite colour is blue");
 	}
 
 }
