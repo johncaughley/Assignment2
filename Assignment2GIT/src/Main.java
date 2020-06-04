78d3f9d189ef25c5cc53322ae68df7e6fb2290ca
johncaughley <johncaughley1@gmail.com>
2020-06-04 18:41:57 +1200

    Version 3(version 4 if including initial upload)

--- Assignment2GIT/src/Main.java
+++ Assignment2GIT/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("Hi, my name is John");
 		System.out.println("My Favourite colour is blue");
 		System.out.println("My Favourite mode of transportation is walking");
+		System.out.println("The best day of the week is Friday");		
 	}
 
 }
